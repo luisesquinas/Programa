5261850d2548d5e9ae6db454300b9892f26f3684
Luis Esquinas <luis.esquinas.aragon@gmail.com>
2024-08-03 13:05:47 +0200

    Archivos iniciales. Aún sin programación mía.

---
+++ Listado compartido/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    // Create and open a text file
+    fstream MyFile("filename.txt");
+
+    //Write to the file
+    MyFile << "Files can be tricky, but it is fun enough!";
+    
+    // Read from the file
+    string myText;
+    getline(Myfile, myText);
+    cout << myText;
+    
+    // Close the file
+    MyFile.close()"
