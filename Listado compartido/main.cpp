a697dec56cc0a372b7dcc48d6d8f9707c2e17511
Luis Esquinas <luis.esquinas.aragon@gmail.com>
2024-08-03 16:00:32 +0200

    Ejercicio completo

--- Listado compartido/main.cpp
+++ Listado compartido/main.cpp
@@ -3,16 +3,38 @@
 using namespace std;
 
 int main() {
-    // Create and open a text file
-    fstream MyFile("filename.txt");
+    ifstream fichero("listadoAlumnos.txt");
+    int accion;
+    string nombre;
+    string linea;
+    string contenido = "";
 
-    //Write to the file
-    MyFile << "Files can be tricky, but it is fun enough!";
+    cout << "LISTADO DE ALUMNOS: \n";
+    while ( getline(fichero, linea ) ) {
+        cout << linea + "\n";
+        contenido += linea + "\n";
+    }
     
-    // Read from the file
-    string myText;
-    getline(Myfile, myText);
-    cout << myText;
-    
+    cout << " - Quieres poner un nombre n uevo? \n";
+    cout << "- 1. Nombre nuevo\n";
+    cout << "- 2. Salir\n";
+    cin >> accion;
+    switch(accion){
+        case 1:
+            cout << "Nuevo nombre: ";
+            cin >> nombre;
+            contenido += nombre + "\n";
+            break;
+        case 2: exit(0);
+        default: exit(0);
+}
+ 
     // Close the file
-    MyFile.close()"
+    fichero.close();
+
+
+    ofstream f("listadoAlumnos.txt");
+    f << contenido;
+    f.close();
+    }
+    
