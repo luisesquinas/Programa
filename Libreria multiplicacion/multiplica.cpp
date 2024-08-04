0160c7a85640d4a547eec14298e2a2458917c62e
Luis Esquinas <luis.esquinas.aragon@gmail.com>
2024-08-04 16:21:04 +0200

    Ahora mismo no está funcionando

---
+++ Libreria multiplicacion/multiplica.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "multiplica.h"
+using namespace std;
+
+int multiplica (int x, int y){
+    int resultado = x * y;
+    return resultado
+}
---
+++ Libreria multiplicacion/multiplica.h
@@ -0,0 +1,6 @@
+#ifndef MULTIPLICA_H
+#define MULTIPLICA_H
+
+int multiplica (int x, int y);
+
+#endif
