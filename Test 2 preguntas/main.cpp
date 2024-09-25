059d9b8383eb5b656ef71233c918429bde8c321e
Luis Esquinas <luis.esquinas.aragon@gmail.com>
2024-09-25 23:48:02 +0200

    Add files via upload

---
+++ Test 2 preguntas/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "test.h"
+using namespace std;
+
+int main(){
+
+    rellenaPreguntas();
+
+    pregunta(1);
+    pregunta(2);
+
+    return 0;
+    }    
---
+++ Test 2 preguntas/test.h
@@ -0,0 +1,9 @@
+#include <string>
+using namespace std;
+
+void rellenaPreguntas();
+void getEnunciado(int n);
+void getRespuestas(int n);
+void checkRespuesta(int n, string r);
+
+void pregunta(int numPregunta);
